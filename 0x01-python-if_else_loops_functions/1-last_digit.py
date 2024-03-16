f0b48945b2b31616ae080b0308c5b1cc2ed47bcd
olanrewaju-ai <zhikrusalam@outlook.com>
2024-03-16 05:55:19 -0700

    All error fixed for lastdigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,11 +8,14 @@ else:
     lastdigit = number % 10
 # Check if last digit is greater than 5
 if lastdigit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastdigit))
+    print("Last digit of {} is {} and is greater than 5"
+            .format(number, lastdigit))
 # Check if its 0
 elif lastdigit == 0:
-    print("Last digit of {} is {} and is 0".format(number, lastdigit))
+    print("Last digit of {} is {} and is 0"
+            .format(number, lastdigit))
 # Check if its not zero and less than 6
 elif lastdigit < 6 and lastdigit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastdigit))
+    print("Last digit of {} is {} and is less than 6 and not 0"
+            .format(number, lastdigit))
 
