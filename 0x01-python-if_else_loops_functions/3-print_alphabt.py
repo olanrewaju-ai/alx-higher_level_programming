9f86d521144e9c30bcb8deeb0a055779dc02921b
olanrewaju-ai <zhikrusalam@outlook.com>
2024-03-17 15:25:07 -0700

    line too lonk error fixed on task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 # Print the ASCII alphabet in lowercase without newline, excluding 'q' and 'e'
-print(''.join(chr(97 + i) for i in range(26) if chr(97 + i) not in ['q', 'e']).format(), end='')
+print(''.join(chr(97 + i) for i in range(26) if chr(97 + i) not in ['q', 'e'])
+        .format(), end='')
