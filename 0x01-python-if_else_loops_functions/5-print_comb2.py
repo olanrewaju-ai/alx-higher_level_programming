0a34a492d51da48d20bbd7d74fbd2042f7298ae1
olanrewaju-ai <zhikrusalam@outlook.com>
2024-03-18 08:11:02 +0100

    task 5 fixed using if else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-# Print numbers from 0 to 99 separated by ', '
-for i in range(100):
-    print('{:02d}'.format(i), end=', ' if i < 99 else '\n')
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
